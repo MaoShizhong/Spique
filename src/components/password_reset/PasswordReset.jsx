1b6b11e6ad1dd22b1ada9e40bd94d7b537816e5c
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2023-10-25 23:12:31 +0100

    Implement password reset
    One-time unique password reset link now successfully changes user
    password.
    Auto-logs out upon success.

--- src/components/password_reset/PasswordReset.jsx
+++ src/components/password_reset/PasswordReset.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { fetchData } from '../../helpers/helpers';
 import { Loading } from '../loading/Loading';
 import styles from './reset.module.css';
@@ -8,9 +8,12 @@ export function PasswordReset() {
     const { resetID } = useParams();
 
     const [isValidToken, setIsValidToken] = useState(false);
-    const [loading, setLoading] = useState(true);
+    const [verifying, setVerifying] = useState(true);
+    const [settingPassword, setSettingPassword] = useState(false);
     const [connectionError, setConnectionError] = useState(false);
     const [passwordErrors, setPasswordErrors] = useState(null);
+
+    const goTo = useNavigate();
 
     useEffect(() => {
         async function verifyToken() {
@@ -22,26 +25,80 @@ export function PasswordReset() {
                 setIsValidToken(true);
             }
 
-            setLoading(false);
+            setVerifying(false);
         }
 
         verifyToken();
     }, [resetID]);
 
+    async function setNewPassword(e) {
+        e.preventDefault();
+        setSettingPassword(true);
+
+        const form = e.target;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+
+        if (password !== confirm) {
+            setPasswordErrors([{ msg: 'Passwords must match' }]);
+            setSettingPassword(false);
+            return;
+        }
+
+        const res = await fetchData(`/auth/password-tokens/${resetID}`, 'PUT', {
+            password,
+            confirm,
+        });
+
+        if (res instanceof Error) {
+            alert('Something went wrong with the server, please try again later.');
+            setSettingPassword(false);
+            goTo('/error');
+        } else if (res.status === 400) {
+            const errors = await res.json();
+            setPasswordErrors(errors);
+            setSettingPassword(false);
+        } else if (!res.ok) {
+            // For catching other error status e.g. 500s
+            setSettingPassword(false);
+            goTo('/error');
+        } else {
+            setSettingPassword(false);
+            alert('Password changed successfully! Please login again.');
+            goTo('/');
+        }
+    }
+
     return (
         <main className={styles.main}>
-            {loading ? (
+            {verifying ? (
                 <Loading text="Verifying link" />
+            ) : settingPassword ? (
+                <Loading text="Setting new password" />
             ) : connectionError ? (
-                <p>Something went wrong with the server, please try again later.</p>
+                <>
+                    <p>Something went wrong with the server, please try again later.</p>
+                    <Link to="/" className={`${styles.return} bg-accented-l`}>
+                        Return to homepage
+                    </Link>
+                </>
             ) : !isValidToken ? (
-                <p>
-                    This link has expired.
-                    <br />
-                    Please request a new password reset link.
-                </p>
+                <>
+                    <p>
+                        This link has expired.
+                        <br />
+                        Please request a new password reset link.
+                    </p>
+                    <Link to="/" className={`${styles.return} bg-accented-lg`}>
+                        Return to homepage
+                    </Link>
+                </>
             ) : (
-                <form>
+                <form className={styles.form} onSubmit={setNewPassword}>
+                    <img src="/spique-full.png" alt="spique logo" className={styles.logo} />
+
+                    <h1>Password reset</h1>
+
                     {passwordErrors && (
                         <ul>
                             {passwordErrors.map((error, i) => (
@@ -51,6 +108,33 @@ export function PasswordReset() {
                             ))}
                         </ul>
                     )}
+
+                    <input
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        aria-label="enter password"
+                        pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$"
+                        onInput={() => setPasswordErrors(null)}
+                        required
+                    />
+
+                    <input
+                        name="confirm"
+                        type="password"
+                        placeholder="Confirm password"
+                        aria-label="confirm password"
+                        pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$"
+                        onInput={() => setPasswordErrors(null)}
+                        required
+                    />
+
+                    <p className={styles.reqs}>
+                        Password must be at least 8 characters and include at least 1 uppercase, 1
+                        lowercase, and 1 number
+                    </p>
+
+                    <button>Set new password</button>
                 </form>
             )}
         </main>
