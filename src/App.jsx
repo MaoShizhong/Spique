50546e14c64a9cc4153f9e3c2967b79d0a95c959
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2023-10-29 00:54:24 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -14,26 +14,27 @@ export default function App() {
     const goTo = useNavigate();
 
     useEffect(() => {
-        async function autoLogin() {
-            const res = await fetchData('/auth/sessions', 'GET');
+        // async function autoLogin() {
+        //     const res = await fetchData('/auth/sessions', 'GET');
 
-            if (res instanceof Error) {
-                alert('Something went wrong with the server, please try again later!');
-            } else if (!res.ok) {
-                goTo('/');
-            } else {
-                setUser(await res.json());
-            }
-        }
+        //     if (res instanceof Error) {
+        //         alert('Something went wrong with the server, please try again later!');
+        //     } else if (!res.ok) {
+        //         goTo('/');
+        //     } else {
+        //         setUser(await res.json());
+        //     }
+        // }
 
-        autoLogin();
+        // autoLogin();
 
-        // Remove #_=_ path in URL when logging in via Facebook
-        if (window.location.hash === '#_=_') {
-            history.replaceState
-                ? history.replaceState(null, null, window.location.href.split('#')[0])
-                : (window.location.hash = '');
-        }
+        // // Remove #_=_ path in URL when logging in via Facebook
+        // if (window.location.hash === '#_=_') {
+        //     history.replaceState
+        //         ? history.replaceState(null, null, window.location.href.split('#')[0])
+        //         : (window.location.hash = '');
+        // }
+        console.log(document.cookie);
     }, [goTo]);
 
     return (
@@ -44,6 +45,16 @@ export default function App() {
             }}
         >
             {user ? <Outlet /> : <Login />}
+            <button
+                style={{ bottom: '3rem', right: '3rem', position: 'fixed' }}
+                onClick={async () => {
+                    const res = await fetchData('/auth/test');
+
+                    console.log(res.status, res);
+                }}
+            >
+                REDIRECT
+            </button>
         </UserContext.Provider>
     );
 }
